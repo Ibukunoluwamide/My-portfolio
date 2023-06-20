0078894f99d7ce3e4daeb9197c1f5e6e316d7d6d
Alao Olamide Ezekiel <125860905+Ibukunoluwamide@users.noreply.github.com>
2023-06-20 16:51:49 +0100

    dark and light switch created

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,23 @@ window.onscroll = function() {
     var top = window.scrollY;
     if(top >=500) {
         header.classList.add('navbarDark');
-        header.classList.add('shadow-lg');
+        switcher.style.color = 'black'
     }
     else {
         header.classList.remove('navbarDark');
-        header.classList.remove('shadow-lg');
+        switcher.style.color = 'white'
     }
+}
+
+const switchMode = ()=> {
+    let body = document.body
+    body.classList.toggle("dark-mode");
+    if (body.className =='dark-mode') {
+        switcher.classList.remove('fa-moon')
+        switcher.classList.add('fa-sun')
+    }else{
+        switcher.classList.remove('fa-sun')
+        switcher.classList.add('fa-moon')
+    }
+
 }
